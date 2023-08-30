2b9be1db9174558c292fce3bc6b2921ed7f9881f
RAKKI R <93214464+iamrakki@users.noreply.github.com>
2023-08-30 18:03:49 +0530

    Update Contest.sol

--- contracts/Contest.sol
+++ contracts/Contest.sol
@@ -34,36 +34,36 @@ contract Contest{
 	modifier validState(PHASE x){
 	    require(state==x);
 	    _;
-	}
+	} 
 
-	constructor() public{
-		admin=msg.sender;
-        state=PHASE.reg;
-		// counter = 0;
+	constructor() public{ 
+		admin=msg.sender; 
+        state=PHASE.reg; 
+		// counter = 0; 
 
-	}
+	} 
 
-    function changeState(PHASE x) onlyAdmin public{
-		require(x > state);
-        state = x;
+    function changeState(PHASE x) onlyAdmin public{ 
+		require(x > state); 
+        state = x; 
     }
 
-	function addContestant(string memory _name , string memory _party , uint _age , string memory _qualification) public onlyAdmin validState(PHASE.reg){
+	function addContestant(string memory _name , string memory _party , uint _age , string memory _qualification) public onlyAdmin validState(PHASE.reg){ 
 		contestantsCount++;
-		contestants[contestantsCount]=Contestant(contestantsCount,_name,0,_party,_age,_qualification);
+		contestants[contestantsCount]=Contestant(contestantsCount,_name,0,_party,_age,_qualification); 
 	}
 
-	function voterRegisteration(address user) public onlyAdmin validState(PHASE.reg){
-		voters[user].isRegistered=true;
+	function voterRegisteration(address user) public onlyAdmin validState(PHASE.reg){ 
+		voters[user].isRegistered=true; 
 	}
 
-	function vote(uint _contestantId) public validState(PHASE.voting){
+	function vote(uint _contestantId) public validState(PHASE.voting){ 
         
-		require(voters[msg.sender].isRegistered);
-		require(!voters[msg.sender].hasVoted);
-        require(_contestantId > 0 && _contestantId<=contestantsCount);
-		contestants[_contestantId].voteCount++;
-		voters[msg.sender].hasVoted=true;
-		voters[msg.sender].vote=_contestantId;
+		require(voters[msg.sender].isRegistered); 
+		require(!voters[msg.sender].hasVoted); 
+        require(_contestantId > 0 && _contestantId<=contestantsCount); 
+		contestants[_contestantId].voteCount++; 
+		voters[msg.sender].hasVoted=true; 
+		voters[msg.sender].vote=_contestantId; 
 	}
 }
